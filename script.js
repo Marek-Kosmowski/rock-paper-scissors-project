6353cf70be0f6a8b4813c796783abee323bd62cf
Marek-Kosmowski <kosmowski.marek@gmail.com>
2022-09-11 21:56:18 +0200

    Add new fuction playRound which also containes rules of the game

--- script.js
+++ script.js
@@ -4,5 +4,34 @@ function getComputerChoice() {
     return choice;
 }
 
+// Game rules
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lose! Paper beats rock!';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win! Rock beats scissors!';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper beats rock!'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lose! Scissors beats paper!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lose! Rock beats scissors!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win! Scissors beats paper!'
+    } else if (playerSelection === computerSelection) {
+        return 'Tie!'
+    } else {
+        return 'Something went wrong!'
+    }
+}
 
-console.log(getComputerChoice());
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+console.log(computerSelection);
