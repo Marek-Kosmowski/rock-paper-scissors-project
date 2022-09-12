71e55e64d24b8f874081bfdb376f46079d2bcbf9
Marek-Kosmowski <kosmowski.marek@gmail.com>
2022-09-12 13:06:30 +0200

    Add global variables to store a score for player and computer. Add return in playRound function so if player or computer wins a round it will increment specific score depending on who won. Change part of code in game function, so it is more clear and organized.

--- script.js
+++ script.js
@@ -5,24 +5,28 @@ function getComputerChoice() {
 }
 
 // Game rules
+let scorePlayer = 0;
+let scoreComputer = 0;
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return 'You lose! Paper beats rock!';
+        return scoreComputer++;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return 'You win! Rock beats scissors!';
+        return scorePlayer++;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'You win! Paper beats rock!'
+        return scorePlayer++;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return 'You lose! Scissors beats paper!'
+        return scoreComputer++;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return 'You lose! Rock beats scissors!'
+        return scoreComputer++;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'You win! Scissors beats paper!'
+        return scorePlayer++;
     } else if (playerSelection === computerSelection) {
         return 'Tie!'
     } else {
         return 'Something went wrong!'
     }
+
 }
 
 
@@ -30,17 +34,22 @@ function game() {
     for (let i = 0; i < 5; i++) {
         let playerSelection = prompt('Pick rock, paper or scissors')
         let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection.toLowerCase(), computerSelection), `game${i + 1}`);
+        playRound(playerSelection, computerSelection);
+        console.log(`Game number: ${i + 1}`)
+        console.log(`Player picked: ${playerSelection}`);
+        console.log(`Computer picked: ${computerSelection}`)
+
+        console.log(`Player score: ${scorePlayer}`);
+        console.log(`Computer score: ${scoreComputer}`);
+        console.log('***************************')
+    }
+    if (scorePlayer > scoreComputer) {
+        console.log('Player wins the game!');
+    } else if (scorePlayer < scoreComputer) {
+        console.log(`Computer wins the game!`);
+    } else {
+        console.log(`It's a tie!`);
     }
 }
 
 game();
-// const playerSelection = prompt('Pick: rock, paper or scissors');
-// const computerSelection = getComputerChoice();
-
-
-// console.log(playRound(playerSelection, computerSelection));
-
-
-
-// console.log(computerSelection);
