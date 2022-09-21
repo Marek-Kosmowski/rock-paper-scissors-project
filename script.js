60acaea0bcfec198ada81a24e7614077e3baf0c1
Marek-Kosmowski <kosmowski.marek@gmail.com>
2022-09-21 18:36:19 +0200

    Add three buttons in index.html so player can choose what he plays by clicking a button. Delete game function, because for now when player clicks button it calls for a playRound function and displayes in console who won.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const buttons = document.querySelectorAll('.btn');
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors']
     const choice = choices[Math.floor(Math.random() * choices.length)]
@@ -8,48 +10,56 @@ function getComputerChoice() {
 let scorePlayer = 0;
 let scoreComputer = 0;
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    computerSelection = getComputerChoice();
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return scoreComputer++;
+        console.log('comp won'), scoreComputer++;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return scorePlayer++;
+        console.log('player won'), scorePlayer++;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return scorePlayer++;
+        console.log('player won'), scorePlayer++;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return scoreComputer++;
+        console.log('comp won'), scoreComputer++;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return scoreComputer++;
+        console.log('comp won'), scoreComputer++;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return scorePlayer++;
+        console.log('player won'), scorePlayer++;
     } else if (playerSelection === computerSelection) {
-        return 'Tie!'
+        console.log('Tie!')
+
     } else {
-        return 'Something went wrong!'
+        console.log()
+        'Something went wrong!'
     }
 
 }
 
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Pick rock, paper or scissors')
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        console.log(`Game number: ${i + 1}`)
-        console.log(`Player picked: ${playerSelection}`);
-        console.log(`Computer picked: ${computerSelection}`)
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         let playerSelection = prompt('Pick rock, paper or scissors')
+//         let computerSelection = getComputerChoice();
+//         playRound(playerSelection, computerSelection);
+//         console.log(`Game number: ${i + 1}`)
+//         console.log(`Player picked: ${playerSelection}`);
+//         console.log(`Computer picked: ${computerSelection}`)
 
-        console.log(`Player score: ${scorePlayer}`);
-        console.log(`Computer score: ${scoreComputer}`);
-        console.log('***************************')
-    }
-    if (scorePlayer > scoreComputer) {
-        console.log('Player wins the game!');
-    } else if (scorePlayer < scoreComputer) {
-        console.log(`Computer wins the game!`);
-    } else {
-        console.log(`It's a tie!`);
-    }
-}
+//         console.log(`Player score: ${scorePlayer}`);
+//         console.log(`Computer score: ${scoreComputer}`);
+//         console.log('***************************')
+//     }
+//     if (scorePlayer > scoreComputer) {
+//         console.log('Player wins the game!');
+//     } else if (scorePlayer < scoreComputer) {
+//         console.log(`Computer wins the game!`);
+//     } else {
+//         console.log(`It's a tie!`);
+//     }
+// }
 
-game();
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        playRound(btn.value);
+    })
+})
